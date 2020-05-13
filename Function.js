f7dc3d3a9879631e1949865ce1c663fff0728ee1
Glen Redden <glen.redden@studytafense.edu.au>
2020-05-13 14:36:07 +1000

    first commit

---
+++ Function.js
@@ -0,0 +1,14 @@
+function addNumber(num1, num2) {
+    console.log(num);
+}
+
+addNumbers(12, 19);
+
+function sayHello(){
+    collection.log("Hello");
+}
+
+sayHello(); {
+    return addNumbers(num, num);
+}
+
---
+++ Objects.js
@@ -0,0 +1,29 @@
+let myDog = new Object ();
+ myDog = {
+    name: "Rex",
+    age: 6
+};
+
+console.log(myDog.name);
+myDog.name = "Buffy";
+console.log(myDog.name);
+myDog.owner ="Glen";
+console.log(myDog);
+
+let propertyIwantToLog = "age";
+console.log(myDog[propertyIwantToLog])
+
+let human = new Object();
+human = {
+    name: "glen",
+    age: 19
+}
+
+human.name = "trent"
+
+console.log(human.name)
+
+
+
+
+
